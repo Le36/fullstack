9a3f730c01f6291a1006e154af7682c487625cf7
Le36 <48809753+Le36@users.noreply.github.com>
2023-01-06 08:44:37 +0200

    4.5*: apufunktioita ja yksikkötestejä, step3

--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -6,6 +6,15 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((mostLikes, current) => {
+        if (current.likes > mostLikes.likes) {
+            return current;
+        }
+        return mostLikes;
+    });
+}
+
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 }
