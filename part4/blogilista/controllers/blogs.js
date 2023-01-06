64192c5aa5396105a91c283b46322f6d2b8ebf9e
Le36 <48809753+Le36@users.noreply.github.com>
2023-01-06 16:29:42 +0200

    4.17: blogilistan laajennus, step5

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require("../models/user");
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
@@ -7,10 +8,23 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+    const body = request.body
+
+    const user = await User.findById(body.userId)
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+    })
 
     const savedBlog = await blog.save()
-    response.json(savedBlog)
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.json(savedBlog.toJSON())
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
