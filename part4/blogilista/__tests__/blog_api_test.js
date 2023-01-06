92ed6df6097b7e7350c52762c6fc5a65fef8fe78
Le36 <48809753+Le36@users.noreply.github.com>
2023-01-06 12:10:31 +0200

    4.9*: blogilistan testit, step2

--- part4/blogilista/__tests__/blog_api_test.js
+++ part4/blogilista/__tests__/blog_api_test.js
@@ -24,6 +24,13 @@ test('correct amount of blogs returned', async () => {
     expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('blog has field id and not _id', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body[0].id).toBeDefined()
+    expect(response.body[0]._id).not.toBeDefined()
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
