fff9f35cbe87449eb2e9f7fd5914b60cbbfec7a8
Le36 <48809753+Le36@users.noreply.github.com>
2023-01-07 13:52:12 +0200

    5.14: blogilistan testit, step2

--- part5/blogilista/src/components/Blog.test.js
+++ part5/blogilista/src/components/Blog.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import {render, screen} from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 
 test('show only author and title, no url', () => {
@@ -16,6 +17,33 @@ test('show only author and title, no url', () => {
     screen.getByText('titlePart', {exact: false})
     screen.getByText('authorPart', {exact: false})
 
-    const element = screen.queryByText('urlPart', {exact: false})
-    expect(element).toBeNull()
+    const urlQuery = screen.queryByText('urlPart', {exact: false})
+    expect(urlQuery).toBeNull()
+    const likesQuery = screen.queryByText('likes', {exact: false})
+    expect(likesQuery).toBeNull()
+})
+
+test('show all when view is pressed', async () => {
+    const blog = {
+        author: 'authorPart',
+        title: 'titlePart',
+        url: 'urlPart',
+        user: {
+            username: 'namePart'
+        }
+    }
+
+    const mockHandler = jest.fn()
+
+    render(<Blog receivedBlog={blog} updateLike={mockHandler} user={mockHandler} remove={mockHandler}/>)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    screen.getByText('titlePart', {exact: false})
+    screen.getByText('authorPart', {exact: false})
+    screen.getByText('urlPart', {exact: false})
+    screen.getByText('namePart', {exact: false})
+    screen.getByText('likes', {exact: false})
 })
