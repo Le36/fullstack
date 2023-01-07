1f6ac2cf1e19a4036f662ca15ad21f186012cd9c
Le36 <48809753+Le36@users.noreply.github.com>
2023-01-07 05:17:25 +0200

    5.1: blogilistan frontend, step1

---
+++ part5/blogilista/src/services/blogs.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const baseUrl = '/api/blogs'
+
+let token = null
+
+const setToken = newToken => {
+    token = `bearer ${newToken}`
+}
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const blogService = {getAll, setToken}
+export default blogService
