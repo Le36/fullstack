324f9b6f6ef4ce9a962e30cf70c22b47ece5cda2
Le36 <48809753+Le36@users.noreply.github.com>
2023-01-07 08:11:26 +0200

    5.7* blogilistan frontend, step7

--- part5/blogilista/src/App.js
+++ part5/blogilista/src/App.js
@@ -7,7 +7,6 @@ import Blog from './components/Blog'
 
 const App = () => {
     const [blogs, setBlogs] = useState([])
-
     const [errorMessage, setErrorMessage] = useState(null)
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
@@ -113,7 +112,6 @@ const App = () => {
             )}
         </div>
     )
-
 }
 
 export default App
--- part5/blogilista/src/components/Blog.js
+++ part5/blogilista/src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({blog}) => (
-    <div>
-        {blog.title} {blog.author}
-    </div>
-)
+import {useState} from "react";
+
+const Blog = ({blog}) => {
+
+    const [view, setView] = useState(false)
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    if (view) {
+        return (
+            <div style={blogStyle}>
+                {blog.title} {blog.author}
+                <button onClick={() => setView(false)}>hide</button>
+                <p> {blog.url} </p>
+                <p>Likes: {blog.likes}
+                    <button>like</button>
+                </p>
+                <p>{(blog.user.username)}</p>
+            </div>
+        )
+    }
+    return (
+        <div style={blogStyle}>
+            {blog.title} {blog.author}
+            <button onClick={() => setView(true)}>view</button>
+        </div>
+    )
+}
 
 export default Blog
