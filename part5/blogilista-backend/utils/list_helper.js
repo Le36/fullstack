3821175e31932cf68325128b28fb75a185bf4b78
Le36 <48809753+Le36@users.noreply.github.com>
2023-01-07 16:09:01 +0200

    5.17: blogilistan end to end -testit, step1

---
+++ part5/blogilista-backend/utils/list_helper.js
@@ -0,0 +1,44 @@
+const {countBy, map, maxBy, sumBy, groupBy} = require('lodash')
+
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((mostLikes, current) => {
+        if (current.likes > mostLikes.likes) {
+            return current
+        }
+        return mostLikes
+    })
+}
+
+const mostBlogs = (blogs) => {
+    const authorCounts = map(countBy(blogs, 'author'),
+        (val, key) => ({author: key, blogs: val}))
+
+    return authorCounts.reduce((mostBlogs, current) => {
+        if (current.blogs > mostBlogs.blogs) {
+            return current
+        }
+        return mostBlogs
+    })
+}
+
+const mostLikes = (blogs) => {
+    const authors = groupBy(blogs, 'author')
+
+    const authorLikes = map(authors, (articles, author) => ({
+        author,
+        likes: sumBy(articles, 'likes')
+    }))
+    return maxBy(authorLikes, 'likes')
+}
+
+module.exports = {
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
+}
