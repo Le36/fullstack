3bcd58c4d72db1f6d69b7dc3ab1eb3d539af7f4c
Le36 <48809753+Le36@users.noreply.github.com>
2023-01-07 16:20:50 +0200

    5.18: blogilistan end to end -testit, step2

--- part5/blogilista/cypress/e2e/blog_app.cy.js
+++ part5/blogilista/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,30 @@
-describe('Blog app', function() {
-    beforeEach(function() {
+describe('Blog app', function () {
+    beforeEach(function () {
         cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        cy.request('POST', 'http://localhost:3003/api/users', {
+            username: 'name', password: 'pass', name: 'test-name'
+        })
+        // create here a user to backend
         cy.visit('http://localhost:3000')
     })
 
-    it('Login form is shown', function() {
+    it('Login form is shown', function () {
         cy.contains('Log in to application')
+    })
+
+    describe('Login', function () {
+        it('succeeds with correct credentials', function () {
+            cy.get('#username').type('name')
+            cy.get('#password').type('pass')
+            cy.get('#login-button').click()
+            cy.contains('test-name logged in')
+        })
+
+        it('fails with wrong credentials', function () {
+            cy.get('#username').type('fail')
+            cy.get('#password').type('fail')
+            cy.get('#login-button').click()
+            cy.contains('wrong credentials')
+        })
     })
 })
--- part5/blogilista/src/App.js
+++ part5/blogilista/src/App.js
@@ -98,15 +98,15 @@ const App = () => {
         <form onSubmit={handleLogin}>
             <div>
                 username
-                <input type="text" value={username} name="Username"
+                <input id='username' type="text" value={username} name="Username"
                     onChange={({target}) => setUsername(target.value)}/>
             </div>
             <div>
                 password
-                <input type="password" value={password} name="Password"
+                <input id='password' type="password" value={password} name="Password"
                     onChange={({target}) => setPassword(target.value)}/>
             </div>
-            <button type="submit">login</button>
+            <button id='login-button' type="submit">login</button>
         </form>
     )
 
