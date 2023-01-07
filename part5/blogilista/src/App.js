39f57e4bc2def79ce5734b4c5d6bcb0178429018
Le36 <48809753+Le36@users.noreply.github.com>
2023-01-07 05:26:50 +0200

    5.2: blogilistan frontend, step2

--- part5/blogilista/src/App.js
+++ part5/blogilista/src/App.js
@@ -16,6 +16,15 @@ const App = () => {
         )
     }, [])
 
+    useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+        if (loggedUserJSON) {
+            const user = JSON.parse(loggedUserJSON)
+            setUser(user)
+            blogService.setToken(user.token)
+        }
+    }, [])
+
 
     const handleLogin = async (event) => {
         event.preventDefault()
@@ -23,7 +32,9 @@ const App = () => {
             const user = await loginService.login({
                 username, password,
             })
-
+            window.localStorage.setItem(
+                'loggedBlogAppUser', JSON.stringify(user)
+            )
             blogService.setToken(user.token)
             setUser(user)
             setUsername('')
@@ -66,7 +77,13 @@ const App = () => {
         <div>
             {errorMessage}
             <h2>blogs</h2>
-            <p>{user.name} logged in</p>
+            <p>{user.name} logged in
+                <button onClick={() => {
+                    window.localStorage.clear()
+                }
+                }>logout
+                </button>
+            </p>
             {blogs.map(blog =>
                 <Blog key={blog.id} blog={blog}/>
             )}
