5872ee9a234364fd390fd3c7617e0dff46988b70
Le36 <48809753+Le36@users.noreply.github.com>
2023-01-08 10:41:25 +0200

    6.19 anekdootit ja connect, step1

--- part6/redux-anecdotes-main/src/components/Notification.js
+++ part6/redux-anecdotes-main/src/components/Notification.js
@@ -1,19 +1,22 @@
-import {useSelector} from 'react-redux'
+import {connect} from 'react-redux'
 
-const Notification = () => {
-
-    const notification = useSelector(state => state.notification)
-
+const Notification = (props) => {
     const style = {
         border: 'solid',
         padding: 10,
         borderWidth: 1
     }
+
     return (
         <div style={style}>
-            {notification}
+            {props.notification}
         </div>
     )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+    return {notification: state.notification}
+}
+
+const ConnectedNotifications = connect(mapStateToProps)(Notification)
+export default ConnectedNotifications
