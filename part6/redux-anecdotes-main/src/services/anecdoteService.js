03f93120a6c2b6a234dfd6faf1e3fcb99b6daff9
Le36 <48809753+Le36@users.noreply.github.com>
2023-01-08 09:16:51 +0200

    6.14 anekdootit ja backend, step2

--- part6/redux-anecdotes-main/src/services/anecdoteService.js
+++ part6/redux-anecdotes-main/src/services/anecdoteService.js
@@ -8,7 +8,7 @@ const getAll = async () => {
 }
 
 const createNew = async (content) => {
-    const object = {content, important: false}
+    const object = {content}
     const response = await axios.post(baseUrl, object)
     return response.data
 }
