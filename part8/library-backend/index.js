c4240daf00027fee5766dbdf7160a98c2d696b20
Le36 <48809753+Le36@users.noreply.github.com>
2023-01-10 20:03:07 +0200

    8.4: kirjailijan kirjat

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -97,7 +97,7 @@ const typeDefs = gql`
     type Query {
         bookCount: Int!
         authorCount: Int!
-        allBooks: [Book!]!
+        allBooks(author: String): [Book!]!
         allAuthors: [Author!]!
     }
 `
@@ -106,7 +106,12 @@ const resolvers = {
     Query: {
         bookCount: () => books.length,
         authorCount: () => authors.length,
-        allBooks: () => books,
+        allBooks: (root, args) => {
+            if (args.author) {
+                return books.filter(b => b.author === args.author)
+            }
+            return books
+        },
         allAuthors: () => authors
     },
     Author: {
