2e08db292bc86c3084bfda40e225c3f050d33bdb
Le36 <48809753+Le36@users.noreply.github.com>
2023-01-10 20:23:14 +0200

    8.7: Kirjailijan syntymävuoden päivitys

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -109,6 +109,10 @@ const typeDefs = gql`
             author: String!
             genres: [String!]!
         ): Book
+        editAuthor(
+            name: String!
+            setBornTo: Int!
+        ): Author
     }
 `
 
@@ -148,6 +152,14 @@ const resolvers = {
                 authors = authors.concat(author)
             }
             return book
+        },
+        editAuthor: (root, args) => {
+            const author = authors.find(a => a.name === args.name)
+            if (!author) return null
+
+            const updatedAuthor = {...author, born: args.setBornTo}
+            authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+            return updatedAuthor
         }
     }
 }
