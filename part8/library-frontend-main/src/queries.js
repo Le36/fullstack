989bbd774fd4a6e33ec86891d8489bf9039caa54
Le36 <48809753+Le36@users.noreply.github.com>
2023-01-11 21:28:06 +0200

    8.8: Kirjailijoiden näkymä

---
+++ part8/library-frontend-main/src/queries.js
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client'
+
+
+export const ALL_AUTHORS = gql`
+    query {
+        allAuthors {
+            name
+            born
+            bookCount
+            id
+        }
+    }
+`
